74ee3a3b06365a56ef11d280418b2473fa4188b5
luiquecruz <luiquecruz@gmail.com>
2024-02-10 18:32:10 -0300

    apply code pattern

--- first-app/express/server.js
+++ first-app/express/server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Express')
-})
+  res.send('Hello Express');
+});
 
 app.listen(port, () => {
-  console.log(`App runing on port ${port}`)
-})
+  console.log(`App runing on port ${port}`);
+});
